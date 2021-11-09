a1b6aa564d6a3e89f2028b64a4a4ef22caf8c827
Cryptopragmatic <tristan.alberio@gmail.com>
2021-11-09 19:47:33 +0800

    window wallet

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -6,6 +7,39 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  /*Our function here is checking the window object in our DOM to see if the Phantom
+   Wallet extension has injected the solana object. If we do have a solana object, we can also check to
+  see if it's a Phantom Wallet.*/
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+  
+      if (solana) {
+        if (solana.isPhantom { {
+          console.log('Phantom wallet found!');
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  /*In React, the useEffect hook gets called once on component mount when that second parameter (the []) is empty.
+  . As soon as someone goes to our app, we can check to see if they have Phantom Wallet installed or not.*/
+
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
