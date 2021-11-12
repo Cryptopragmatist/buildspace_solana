9207a97773a87d6468b5a9e8d8bdb36fbc4440fa
Cryptopragmatic <tristan.alberio@gmail.com>
2021-11-12 16:21:23 +0800

    adding gifts and submit button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import './App.css';
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+const TEST_GIFS = [
+	'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
+	'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
+	'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
+	'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+]
+
 const App = () => {
 
   /*Our function here is checking the window object in our DOM to see if the Phantom
@@ -14,6 +21,8 @@ const App = () => {
 
   //State
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [gifList, setGifList] = useState([]);
   
 
  //actions
@@ -55,6 +64,20 @@ const App = () => {
     setWalletAddress(response.publicKey.toString());
   }
   };
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log('Gif link:', inputValue);
+    } else {
+      console.log('Empty input. Try again.');
+    }
+  };
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+
   //pop up ui for connecting user wallet to the app
   const renderNotConnectedContainer = () => (
     <button
@@ -63,6 +86,33 @@ const App = () => {
     >
       Connect to Wallet
     </button>
+  );
+
+
+//creating a new function called renderConnectedContainer ,simple UI code that will map through all our GIF links and render them
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      {/* Go ahead and add this input and button to start */}
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        sendGif();
+      }}
+    >
+      <input type="text" placeholder="Enter gif link!" 
+      value={inputValue} onChange={onInputChange}  />
+      <button type="submit" className="cta-button submit-gif-button">Submit</button>
+    </form>
+      <div className="gif-grid">
+        {/* Map through gifList instead of TEST_GIFS */}
+
+        {gifList.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 
   /*In React, the useEffect hook gets called once on component mount when that second parameter (the []) is empty.
@@ -78,6 +128,17 @@ const App = () => {
     return () => window.removeEventListener('load', onLoad);
   }, []);
 
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching GIF list...');
+      
+      // Call Solana program here.
+  
+      // Set state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
+
   return (
     <div className="App">
 			{/* This was solely added for some styling fanciness */}
@@ -85,10 +146,12 @@ const App = () => {
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            View your GIF collection in the metaverse ✨ BLOOD GOD!
           </p>
           {/* Add the condition to show this only if we don't have a wallet address */}
           {!walletAddress && renderNotConnectedContainer()}
+          {/* We just need to add the inverse here! */}
+        {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
