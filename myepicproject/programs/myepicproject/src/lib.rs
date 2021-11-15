834f47be9b5e8c6d57c156516b260436275a8e2f
Cryptopragmatic <tristan.alberio@gmail.com>
2021-11-15 20:05:34 +0800

    added simple basic stuff

---
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -0,0 +1,17 @@
+use anchor_lang::prelude::*; //import anchor
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS"); //program id
+
+#[program] 
+// #[porgram] are macros — and they basically attach code to our module. It's sorta like "inheriting" a class.
+//module below is the program, create handlers to call these functions
+// RPC request handlers that we will be able to call from a client application to interact with the program 
+pub mod myepicproject {
+    use super::*;
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
