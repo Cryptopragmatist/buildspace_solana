ca528657b2b14eb6f4c068a77646d8163b77e590
Cryptopragmatic <tristan.alberio@gmail.com>
2021-11-25 23:17:12 +0800

    added some functionalities, not yet implemented

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -27,16 +27,62 @@ pub mod myepicproject {
   let item = ItemStruct {
     gif_link: gif_link.to_string(),
     user_address: *user.to_account_info().key,
+    votes: 0,
+    voters: [].to_vec(),
   };
   // Add it to the gif_list vector.
   base_account.gif_list.push(item);
   base_account.total_gifs += 1;
   Ok(())
 }
+pub fn update_item(ctx: Context<UpdateItem>, gif_link: String) -> ProgramResult {
+  let base_account = &mut ctx.accounts.base_account;
+  let user = &mut ctx.accounts.user;
+  
+  let mut update = true;
+  let mut index = 0;
+  for (i, x) in base_account.gif_list.iter().enumerate() {
+      if x.gif_link == gif_link {
+          if x.voters.contains(&*user.to_account_info().key) {
+              update = false;
+          }
+          index = i;
+          
+      };
+  };
+  
+  if update {
+      base_account.gif_list[index].votes += 1;
+      base_account.gif_list[index].voters.push(*user.to_account_info().key);
+  }
+  
+  Ok(())
+}
+pub fn send_tip(ctx: Context<SendTip>, amount: u64) -> ProgramResult {
+  let from_account = &mut ctx.accounts.user;
+  let to_account = &mut ctx.accounts.to;
+
+  let ix = anchor_lang::solana_program::system_instruction::transfer(
+      &from_account.key(),
+      &to_account.key(),
+      amount,
+  );
+
+  anchor_lang::solana_program::program::invoke(
+      &ix,
+      &[
+          from_account.to_account_info(),
+          to_account.to_account_info(),
+      ]
+  );
+  
+  Ok(())
+}
+}
 
   
 
-}
+
   
 
 
@@ -70,12 +116,31 @@ pub struct AddGif<'info> {
   pub user: Signer<'info>,
   }
 
+  #[derive(Accounts)]
+  pub struct UpdateItem<'info> {
+      #[account(mut)]
+      pub base_account: Account<'info, BaseAccount>,
+      #[account(mut)]
+      pub user: Signer<'info>,
+  }
+
+  #[derive(Accounts)]
+pub struct SendTip<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
+
 // Create a custom struct for us to work with.
 //serialize our data into binary format before storing it,deserialize to retrieve it
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct ItemStruct {
     pub gif_link: String,
     pub user_address: Pubkey,
+    pub votes: u32,
+    pub voters: Vec<Pubkey>,
 }
 
 // Tell Solana what we want to store on this account.
